5d3caedcef087ec5a1a7d6b8ff9a1866175ab36f
yourijanssen <64319462+yourijanssen@users.noreply.github.com>
2023-11-16 14:14:36 +0100

    almost done

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -2,41 +2,41 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css'],
+    selector: 'app-projects',
+    templateUrl: './projects.component.html',
+    styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-  projects = [
-    {
-      name: 'Forum page',
-      description:
-        'A forum page where users can post and comment on posts related to Sustainability.',
-      technologies: 'React, JavaScript, CSS',
-      url: 'https://example.com/project2',
-      img1: 'assets/AngularAmigosSite/angularAmgigosPage1.png',
-      img2: 'assets/AngularAmigosSite/AngularAmgigosPage2.png',
-      img3: 'assets/AngularAmigosSite/AngularAmigosPage3.png',
-    },
-    {
-      name: 'Boat Rental',
-      description: 'A webshop where users can rent boats.',
-      technologies:
-        'Angular, TypeScript, CSS, Node.js, Express.js, MySQL, Sequelize, Sinon, Mocha, Chai, Git, GitKeb, Thunderclient',
-      url: 'https://example.com/project2',
-      img1: 'assets/BoatRentelSite/BootverhuurP1.png',
-      img2: 'assets/BoatRentelSite/BootverhuurP2.png',
-      img3: 'assets/BoatRentelSite/BootverhuurP3.png',
-    },
-    {
-      name: 'Crowdfunding page',
-      description: 'A page where users can donate money for the care of lamas.',
-      technologies: 'React, JavaScript, CSS',
-      url: 'https://example.com/project2',
-      img1: 'assets/MayoLamaSite/MayoLamaP1.png',
-      img2: 'assets/MayoLamaSite/MayoLamaP2.png',
-      img3: 'assets/MayoLamaSite/MayoLamaP3.png',
-    },
-    // Add more projects as needed
-  ];
+    projects = [
+        {
+            name: 'Forum page',
+            description:
+                'A forum page where users can post and comment on posts related to Sustainability.',
+            technologies: 'React, JavaScript, CSS',
+            url: 'https://github.com/yourijanssen/AngularAmigos',
+            img1: 'assets/AngularAmigosSite/angularAmgigosPage1.png',
+            img2: 'assets/AngularAmigosSite/AngularAmgigosPage2.png',
+            img3: 'assets/AngularAmigosSite/AngularAmigosPage3.png'
+        },
+        {
+            name: 'Boat Rental',
+            description: 'A webshop where users can rent boats.',
+            technologies:
+                'Angular, TypeScript, CSS, Node.js, Express.js, MySQL, Sequelize, Sinon, Mocha, Chai, Git, GitKeb, Thunderclient',
+            url: 'https://github.com/yourijanssen/bootverhuur',
+            img1: 'assets/BoatRentelSite/BootverhuurP1.png',
+            img2: 'assets/BoatRentelSite/BootverhuurP2.png',
+            img3: 'assets/BoatRentelSite/BootverhuurP3.png'
+        },
+        {
+            name: 'Crowdfunding page',
+            description: 'A page where users can donate money for the care of lamas.',
+            technologies: 'React, JavaScript, CSS',
+            url: 'https://github.com/yourijanssen/MayoLama',
+            img1: 'assets/MayoLamaSite/MayoLamaP1.png',
+            img2: 'assets/MayoLamaSite/MayoLamaP2.png',
+            img3: 'assets/MayoLamaSite/MayoLamaP3.png'
+        }
+        // Add more projects as needed
+    ];
 }
