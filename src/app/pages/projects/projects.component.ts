a9b67e3d63884a067e4b55e9bbc93e6e6beef95e
yourijanssen <64319462+yourijanssen@users.noreply.github.com>
2023-11-16 11:14:42 +0100

    prettier + photos

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -14,6 +14,9 @@ export class ProjectsComponent {
         'A forum page where users can post and comment on posts related to Sustainability.',
       technologies: 'React, JavaScript, CSS',
       url: 'https://example.com/project2',
+      img1: 'assets/AngularAmigosSite/angularAmgigosPage1.png',
+      img2: 'assets/AngularAmigosSite/AngularAmgigosPage2.png',
+      img3: 'assets/AngularAmigosSite/AngularAmigosPage3.png',
     },
     {
       name: 'Boat Rental',
@@ -21,13 +24,18 @@ export class ProjectsComponent {
       technologies:
         'Angular, TypeScript, CSS, Node.js, Express.js, MySQL, Sequelize, Sinon, Mocha, Chai, Git, GitKeb, Thunderclient',
       url: 'https://example.com/project2',
+      img1: 'assets/BoatRentelSite/BootverhuurP1.png',
+      img2: 'assets/BoatRentelSite/BootverhuurP2.png',
+      img3: 'assets/BoatRentelSite/BootverhuurP3.png',
     },
     {
-      name: 'Collection page',
-      description:
-        'A page where users can view and edit their collection of items.',
+      name: 'Crowdfunding page',
+      description: 'A page where users can donate money for the care of lamas.',
       technologies: 'React, JavaScript, CSS',
       url: 'https://example.com/project2',
+      img1: 'assets/MayoLamaSite/MayoLamaP1.png',
+      img2: 'assets/MayoLamaSite/MayoLamaP2.png',
+      img3: 'assets/MayoLamaSite/MayoLamaP3.png',
     },
     // Add more projects as needed
   ];
