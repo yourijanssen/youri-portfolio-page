2067270fd5f79765f4bb6cb3cb640fa7de0f430c
yourijanssen <64319462+yourijanssen@users.noreply.github.com>
2023-11-17 13:57:14 +0100

    lsat updates

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'],
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  isMenuOpen: boolean = false;
+    isMenuOpen: boolean = false;
 
-  toggleMenu(): void {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
+    constructor(private renderer: Renderer2) {}
 
-  closeMenu(): void {
-    this.isMenuOpen = false;
-  }
+    toggleMenu(): void {
+        this.isMenuOpen = !this.isMenuOpen;
+
+        // Toggle the 'menu-open' class on the body
+        if (this.isMenuOpen) {
+            this.renderer.addClass(document.body, 'menu-open');
+        } else {
+            this.renderer.removeClass(document.body, 'menu-open');
+        }
+    }
+
+    closeMenu(): void {
+        this.isMenuOpen = false;
+
+        // Remove the 'menu-open' class on closing the menu
+        this.renderer.removeClass(document.body, 'menu-open');
+    }
 }
